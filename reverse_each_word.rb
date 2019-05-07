8652e29099637ca72410df600d76de984b2d8cca
Scott Gale <galejscott@gmail.com>
2019-05-07 17:44:31 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -3,3 +3,10 @@ def reverse_each_word(sentence)
     word.reverse 
   end
 end
+
+def reverse_each_word(sentence)
+  reversed = sentence.split(" ").collect do |word|
+  word.reverse
+end
+  reversed.join(" ")
+end
